e3d7253f7cc56d175ba39552dbfe1b6051bc71f0
antoinebrunet1 <antoinebrunet1@outlook.com>
2024-02-28 10:36:08 -0500

    Changing constansts file for questions

--- src/app/question/service/constant/constant.ts
+++ src/app/question/service/constant/constant.ts
@@ -1,7 +1,7 @@
 export const Constant = {
     API_END_POINT: 'http://localhost:8080/',
     METHODS: {
-        SAVE_QUESTION: 'question',
-        GET_ALL_QUESTIONS: 'questions'
+        SAVE_QUESTION: 'addQuestion',
+        GET_ALL_QUESTIONS: 'getAllQuestions'
     }
 }
